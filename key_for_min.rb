81e6fb31d4a899366920ccf082a87e43a7a9d228
Wills Blake <richwblake@gmail.com>
2019-11-08 03:40:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,7 @@
 
 def key_for_min_value(name_hash)
   return nil if name_hash.empty?
+  names_arr = name_hash.to_a
   lowest_value = names_arr[0][1]
   lowest_key = names_arr[0][0]
   p lowest_value
