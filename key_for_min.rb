67b3c3973655019fc6630bb28f246c686ca91605
Wills Blake <richwblake@gmail.com>
2019-11-08 03:10:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  names_arr = name_hash.to_a
+  lowest_value = names_arr[0][0]
+  p lowest_value
+  
+ 
 end
